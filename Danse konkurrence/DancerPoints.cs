b8e79e018e7dc63298bbfa81b07abbfef4305b23
Nicklas <nicklashelsberg@gmail.com>
2022-07-11 09:53:13 +0200

    Add project files.

---
+++ Danse konkurrence/DancerPoints.cs
@@ -0,0 +1,20 @@
+﻿namespace Danse_konkurrence
+{
+    internal class DancerPoints
+    {
+        internal string navn;
+        internal int points;
+        internal DancerPoints(string navn, int points)
+        {
+            this.navn = navn;
+            this.points = points;
+        }
+        public static DancerPoints operator+ (DancerPoints first, DancerPoints last)
+        {
+            string navn = $"{first.navn} og {last.navn}";
+            int points = first.points + last.points;
+            DancerPoints total = new DancerPoints(navn, points);
+            return total;
+        }
+    }
+}
---
+++ Danse konkurrence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danse_konkurrence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string danser1, danser2;
+                int danserpoint1, danserpoint2;
+
+                Console.Write("Skriv danser 1's navn: ");
+                danser1 = Console.ReadLine();
+                Console.Write("\nSkriv danser 2's navn: ");
+                danser2 = Console.ReadLine();
+                do
+                {
+                    try
+                    {
+                        Console.Write("\nSkriv danser 1's points: ");
+                        danserpoint1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("\nSkriv danser 2's points: ");
+                        danserpoint2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Points skal være i tal");
+                        continue;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                } while (true);
+                DancerPoints dancer1 = new DancerPoints(danser1, danserpoint1);
+                DancerPoints dancer2 = new DancerPoints(danser2, danserpoint2);
+                DancerPoints dansertotal = dancer1 + dancer2;
+
+                Console.WriteLine($"\n{dansertotal.navn}'s totale points er: {dansertotal.points}\n");
+            } while (true);
+            
+
+        }
+    }
+}
